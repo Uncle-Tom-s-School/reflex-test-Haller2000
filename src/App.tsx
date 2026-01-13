7e2fd7ab3b08e7a5da7299e61db999a82ec90fd4
Haller2000 <hallerpeter7@gmail.com>
2026-01-13 11:32:20 +0100

    kesz

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,62 @@
-const App = () => {
-  return (
-    <div>App</div>
-  )
+import { useEffect, useState } from 'react';
+
+function randomSzam(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-export default App
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [megy, setMegy] = useState(false); 
+  const [van, setVan] = useState(true); 
+  const [rnd, setRnd] = useState(randomSzam(1, 2));
+
+
+  useEffect(() => {
+    if (van) {
+      const hideTimeout = setTimeout(() => {
+        setVan(false);
+        setMegy(true); 
+      }, rnd * 1000); 
+
+      return () => clearTimeout(hideTimeout);
+    }
+  }, [van, rnd]);
+
+
+  useEffect(() => {
+    if (megy) {
+      const id = setInterval(() => {
+        setCount((prevCount) => prevCount + 1);
+      }, 1);
+
+      return () => {
+        clearInterval(id);
+      };
+    }
+  }, [megy]);
+
+  return (
+    <div>
+
+      {van && (
+        <div
+          style={{
+            width: '100px',
+            height: '100px',
+            backgroundColor: 'red',
+            marginBottom: '20px',
+          }}
+        >
+          
+        </div>
+      )}
+      
+
+      <div id='ok' onClick={() => setMegy(!megy)}>
+        {count + "ms"}
+      </div>
+    </div>
+  );
+};
+
+export default App;
